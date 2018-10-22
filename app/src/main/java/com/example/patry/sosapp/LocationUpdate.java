7e34b509d2d158b0c3b97d7a90d22276c3972273
Neptun332 <patrykbednarz8@gmail.com>
2018-10-22 22:49:07 +0200

    Changed GPS api (google api), added backgroud service, TODO move service to foreground.

--- app/src/main/java/com/example/patry/sosapp/LocationUpdate.java
+++ app/src/main/java/com/example/patry/sosapp/LocationUpdate.java
@@ -1,83 +1,100 @@
 package com.example.patry.sosapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.widget.TextView;
 
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.logging.Logger;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationUpdate implements LocationListener {
+public class LocationUpdate extends Service implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener  {
 
+    GoogleMap mGoogleMap;
+    SupportMapFragment mapFrag;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
 
-    private final Context context;
-    private final Activity activity;
-
-
-
-    private Location userLocation = new Location("");
-    private LocationManager locationManager;
-
-    private LatLng userPosition;
-    private GoogleMap mMap;
-    private float zoomlvl = 16;
-
-    public LocationUpdate(Context context, Activity activity) {
-        this.context = context;
-        this.activity = activity;
-        this.locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-
-
+    @Override
+    public void onCreate(){
+        buildGoogleApiClient();
     }
 
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {}
 
     @Override
     public void onLocationChanged(Location location) {
-        // Called when a new location is found by the network location provider.
-        userLocation = location;
-        userPosition = new LatLng(userLocation.getLatitude(),userLocation.getLongitude());
-        drawUserPosition();
 
+        Location userLocation = location;
+        if (userLocation != null) {
+            Intent i = new Intent("location_update");
+            i.putExtra("coordinates", new LatLng(location.getLatitude(),location.getLongitude()));
+            sendBroadcast(i);
+        }
     }
+
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-    @Override
-    public void onProviderEnabled(String provider) {}
-
-    @Override
-    public void onProviderDisabled(String provider) {}
-
-    public void setmMap(GoogleMap mMap) {
-        this.mMap = mMap;
-    }
-
-
-    public void drawUserPosition(){
-
-        mMap.addCircle(new CircleOptions().radius(1).center(userPosition));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition,zoomlvl));
-
-    }
-
-    public LatLng getUserPosition() {
-        return userPosition;
-    }
-
-    public Location getUserLocation() {
-        return userLocation;
-    }
-    public void StartUpdatingLocation(){
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, this);
-    }
-    public void StopUpdatingLocation(){
-        locationManager.removeUpdates(LocationUpdate.this);
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
+
+
+
--- app/src/main/java/com/example/patry/sosapp/MapsActivity.java
+++ app/src/main/java/com/example/patry/sosapp/MapsActivity.java
@@ -1,33 +1,64 @@
 package com.example.patry.sosapp;
 
 import android.Manifest;
+import android.app.ActivityManager;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+
 import android.location.Location;
+
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.telecom.ConnectionService;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.security.Provider;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+         {
 
     private LocationUpdate locationUpdate;
 
@@ -42,6 +73,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public int sendSMSEveryInSeconds = 30;
 
 
+    GoogleMap mGoogleMap;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+
+
+
+    private  LocationListener locationListener;
+
+    private BroadcastReceiver broadcastReceiver;
+
+    public static boolean isServiceRunning =false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,9 +94,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-
-
-
+        //locationUpdate = new LocationUpdate(MapsActivity.this, MapsActivity.this);
+        Intent i =new Intent(getApplicationContext(),LocationUpdate.class);
+        startService(i);
 
         final Button settingsButton = findViewById(R.id.settings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
@@ -66,15 +109,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         final Button startButton = findViewById(R.id.start);
         startButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(!startedUpdatingLocation) {
-                    locationUpdate = new LocationUpdate(MapsActivity.this, MapsActivity.this);
-                    locationUpdate.setmMap(mMap);
-                    locationUpdate.StartUpdatingLocation();
+                if (!startedUpdatingLocation) {
+
+                    startService(new Intent(MapsActivity.this, LocationUpdate.class));
+
                     startButton.setText("Stop");
                     startedUpdatingLocation = true;
-                }
-                else{
-                    locationUpdate.StopUpdatingLocation();
+                } else {
+                    startService(new Intent(MapsActivity.this, LocationUpdate.class));
                     startButton.setText("Start");
                     startedUpdatingLocation = false;
                 }
@@ -86,20 +128,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         handler.post(periodicUpdate);
 
 
-
     }
 
 
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera. In this case,
-         * we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to install
-         * it inside the SupportMapFragment. This method will only be triggered once the user has
-         * installed Google Play services and returned to the app.
-         */
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -110,30 +150,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-
-    private Runnable periodicUpdate = new Runnable () {
+    private Runnable periodicUpdate = new Runnable() {
         @Override
         public void run() {
             // scheduled another events to be in 10 seconds later
-            handler.postDelayed(periodicUpdate, sendSMSEveryInSeconds*1000 );
-                    // below is whatever you want to do
+            handler.postDelayed(periodicUpdate, sendSMSEveryInSeconds * 1000);
+            // below is whatever you want to do
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MapsActivity.this);
-        if( preferences.getBoolean("EnableSMSSend",false)==true && preferences.getString("phoneNumber","")!= "") {
-            Location userLocation = locationUpdate.getUserLocation();
-
-            SmsManager.getDefault().sendTextMessage(
-                    "+48"+preferences.getString("phoneNumber",""),
-                    null,
-                    "Latitude: "+userLocation.getLatitude() + " Longitude: " + userLocation.getLongitude() + " Time: " + ConvertTimeToSimpleDateFormat(userLocation.getTime()),
-                    null,
-                    null);
-        }
+//        if( preferences.getBoolean("EnableSMSSend",false)==true && preferences.getString("phoneNumber","")!= "") {
+//            Location userLocation = locationUpdate.getUserLocation();
+//
+//            SmsManager.getDefault().sendTextMessage(
+//                    "+48"+preferences.getString("phoneNumber",""),
+//                    null,
+//                    "Latitude: "+userLocation.getLatitude() + " Longitude: " + userLocation.getLongitude() + " Time: " + ConvertTimeToSimpleDateFormat(userLocation.getTime()),
+//                    null,
+//                    null);
+//        }
 
 
         }
     };
 
-    public void askForSendSMSPermission(){
+    public void askForSendSMSPermission() {
         // Here, thisActivity is the current activity
         if (ContextCompat.checkSelfPermission(MapsActivity.this,
                 Manifest.permission.SEND_SMS)
@@ -160,10 +199,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
     }
+
     public String ConvertTimeToSimpleDateFormat(long time) {
         Date date = new Date(time);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss a");
         return sdf.format(date);
+    }
+
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(broadcastReceiver == null){
+            broadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                   LatLng userPosition =  (LatLng) intent.getExtras().get("coordinates");
+                   Toast.makeText(MapsActivity.this,userPosition.latitude + " " + userPosition.longitude, Toast.LENGTH_LONG).show();
+                    mMap.addCircle(new CircleOptions().radius(1).center(userPosition));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition,16));
+                }
+            };
+        }
+        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+    }
+
+    @Override
+    protected  void onDestroy(){
+        Intent i = new Intent(getApplicationContext(),LocationUpdate.class);
+        stopService(i);
     }
 
 }
